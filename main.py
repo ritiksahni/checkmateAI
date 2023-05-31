36d2b5a9e7ccd205682fe775398ce24e29e5ebd7
Ritik Sahni <ritiksahni0203@gmail.com>
2023-05-31 21:32:49 +0530

    Add ConversationBufferMemory to chain_type_kwargs for RetrievalQA

--- main.py
+++ main.py
@@ -11,57 +11,59 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain.vectorstores import Chroma
 from langchain.llms import OpenAI
-from langchain.chains import (
-    ConversationChain,
-    ConversationalRetrievalChain,
-    RetrievalQA,
-)
+from langchain.chains import RetrievalQA
+
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 
 load_dotenv()
 
 
-def loadContent():
-    loader = RoamLoader("./Roam_DB")
+loader = RoamLoader("./Roam_DB")
 
-    docs = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=100,
-        separators=["\n\n", "\n", ".", ";", ",", " ", ""],
-    )
-    texts = text_splitter.split_documents(docs)
+docs = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=100,
+    separators=["\n\n", "\n", ".", ";", ",", " ", ""],
+)
+texts = text_splitter.split_documents(docs)
 
-    embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
-    instance = Chroma.from_documents(texts, embeddings, persist_directory="./data")
-    template = """You are an AI study partner, you have access to concepts, notes, books, etc. You must ask 3 questions one-by-one in turns. Ask a question, wait for response and respond to that answer with your feedback and the next question. Stop asking questions when 3 are complete but make sure to give feedback to every response based on the notes that you have access to.
+embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
+instance = Chroma.from_documents(texts, embeddings, persist_directory="./data")
+template = """You are an AI study partner. You have access to notes, context of the conversation using which you must help the user learn, remember, think about old and newer ideas.
+Make sure to use subject matter from the context that you have already and only add new information on top of that. Don't pivot the topic of the converation, stick to a limited series of related topics.
 
-    Feel free to give words of encouragement to behave like a good study partner.
+Remember to not ask too many questions, users like good engaging study partners who make them think. Stick to 3 questions, that too, one-by-one, and send a concluding message that's comfortable, humane to read.
 
-    Every message should be related on one random topic exclusively, don't move the conversation to different directions. Just stick to that and ask/answer based on that. But remember, only talk about ideas from the Roam Research notes given to you.
-    
-    With every question, add a "status marker" at the end. Add "expecting information" if you're expecting a response from human on that particular question. Add "concluded" if you've received a response on that question and are just trying to give feedback.
+Initiate conversation humanely, don't jump into the questions or the subject matter directly.
 
-    Don't continue talking for a long time. Ask 3 questions and conclude the chat session. Don't do anything else other than ask questions, add insights on the same topic.
-    {context}
-    Human: {question}
-    AI:
-    """
+All the notes that you're given in the form of embeddings are old written notes, ideas, thoughts. You must not assume they're real-time. However, what is real-time is the context of the conversation.
 
-    llm = OpenAI(temperature=0)
+Use conversation history for context (delimited by <hs></hs>)
 
-    prompt = PromptTemplate(input_variables=["context", "question"], template=template)
+<hs>
+{history}
+</hs>
 
-    chain_type_kwards = {"prompt": prompt}
-    conversation = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=instance.as_retriever(),
-        chain_type_kwargs=chain_type_kwards,
-    )
-    return conversation
+{context}
+Human: {question}
+AI:
+"""
 
+llm = OpenAI(temperature=0)
+memory = ConversationBufferMemory(memory_key="history", input_key="question")
+prompt = PromptTemplate(
+    input_variables=["context", "question", "history"], template=template
+)
+
+chain_type_kwards = {"prompt": prompt, "memory": memory}
+conversation = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=instance.as_retriever(),
+    chain_type_kwargs=chain_type_kwards,
+)
 
 # Getting Bot Token From Secrets
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
@@ -95,7 +97,7 @@ def send_welcome(message):
 # Reply To All Messages
 @bot.message_handler(func=lambda msg: True)
 def all(message):
-    bot.reply_to(message, loadContent().run(message.text))
+    bot.reply_to(message, conversation.run(message.text))
     # bot.reply_to(message, conversation.predict(input=message.text))
     print(
         f"Reply sent. Chat ID - {message.chat.id}, User's name: {message.chat.first_name} {message.chat.last_name}. Message received - {message.text}\n"
